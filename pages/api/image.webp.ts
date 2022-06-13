ba40d8ad57407d1ec9f8d7d07b2e7b2429426ada
pulchra mentis <1747037+pulchra-mentis@users.noreply.github.com>
2022-06-13 16:06:24 -0400

    Restrict ID values

--- pages/api/image.webp.ts
+++ pages/api/image.webp.ts
@@ -6,24 +6,29 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
       const route =
         (req.query.route as string) === 'systems' ? 'systems' : 'members'
-      const data = await fetch(
-        `https://api.pluralkit.me/v2/${route}/${req.query.id as string}`,
-      )
-      const json = await data.json()
-      const initialImage = await fetch(json[req.query.image as string])
-      const initialArrayBuffer = await initialImage.arrayBuffer()
-      const initialBuffer = Buffer.from(initialArrayBuffer)
-      const resizedBuffer = await sharp(initialBuffer)
-        .resize({
-          width: parseInt(req.query.width as string),
-          height: parseInt(req.query.height as string),
-        })
-        .toFormat('webp')
-        .toBuffer()
-      res
-        .setHeader('Content-Type', 'image/webp')
-        .status(200)
-        .send(resizedBuffer)
+      const id = /[a-z]{5}/.test(req.query.id as string)
+        ? (req.query.id as string)
+        : null
+      if (!id) {
+        res.status(404).json({ error: 'Member not found' })
+      } else {
+        const data = await fetch(`https://api.pluralkit.me/v2/${route}/${id}`)
+        const json = await data.json()
+        const initialImage = await fetch(json[req.query.image as string])
+        const initialArrayBuffer = await initialImage.arrayBuffer()
+        const initialBuffer = Buffer.from(initialArrayBuffer)
+        const resizedBuffer = await sharp(initialBuffer)
+          .resize({
+            width: parseInt(req.query.width as string),
+            height: parseInt(req.query.height as string),
+          })
+          .toFormat('webp')
+          .toBuffer()
+        res
+          .setHeader('Content-Type', 'image/webp')
+          .status(200)
+          .send(resizedBuffer)
+      }
     } catch (error) {
       res.status(500).json({ error: (error as Error).message })
     }
